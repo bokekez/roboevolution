aef2b54bf7d0566f60178e9f77497f406690b123
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-19 22:56:32 +0100

    1

--- src/App.js
+++ src/App.js
@@ -134,8 +134,8 @@ if (route === 'home')
 	 		<h1 className='f1'>RoboFriends</h1>
 			{/* <input onClick={() => route !=='search' ? routeChange : setRoute === 'search' && console.log('was clicked')} type="submit" value="Search robots"/>
 			<input onClick={() => route !=='create' ? routeChange : setRoute === 'create'}  type="submit" value="Create robots"/> */}
-			<input className={`'dim' ${colorVar} ${colorVarText}`} style={{ justifyContent: 'center', cursor: 'pointer', borderRadius: '12px'}} onClick={routeChangeSearch} type="submit" value="Search robots"/>
-			<input className={`${colorVar} ${colorVarText}`} style={{ justifyContent: 'center', cursor: 'pointer', borderRadius: '12px'}} onClick={routeChangeCreate} type="submit" value="Create robots"/>
+			<input className={` grow ${colorVar} ${colorVarText}`} style={{ justifyContent: 'center', cursor: 'pointer', borderRadius: '12px'}} onClick={routeChangeSearch} type="submit" value="Search robots"/>
+			<input className={` grow ${colorVar} ${colorVarText}`} style={{ justifyContent: 'center', cursor: 'pointer', borderRadius: '12px'}} onClick={routeChangeCreate} type="submit" value="Create robots"/>
 		</div>
         <footer>
         </footer>
@@ -150,9 +150,9 @@ if (route === 'create')
 			<div className='tc'>	
 				<div style={{display: 'flex', flexDirection: 'row'}}>
 				<Toggle className=' tc' style={{alignSelf: 'center', position: 'absolute'}} theme={theme} toggleTheme={toggleTheme} />
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', top: '1rem', borderRadius: '12px',  }} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', top: '1rem', borderRadius: '12px',  }} 
 				 onClick={routeChangeSearch} type="submit" value="Search"/>
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
 				 onClick={routeChangeHome} type="submit" value="Home"/>
 				</div>
 				<h1 className='f1'>RoboFriends</h1>
@@ -174,9 +174,9 @@ if (state === 'initial')
         <div className='tc'>
 				<div style={{display: 'flex', flexDirection: 'row'}}>
 				<Toggle className=' tc' style={{alignSelf: 'center', position: 'absolute'}} theme={theme} toggleTheme={toggleTheme} />
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', height: '1rem', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', height: '1rem', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
 				 onClick={routeChangeCreate} type="submit" value="Create"/>
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', height: '1rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', height: '1rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
 				 onClick={routeChangeHome} type="submit" value="Home"/>
 				</div>
 	 		<h1 className='f1'>RoboFriends</h1>
@@ -198,9 +198,9 @@ else if (state === 'loaded') {
 			<div className='tc'>	
 				<div style={{display: 'flex', flexDirection: 'row'}}>
 				<Toggle className=' tc' style={{alignSelf: 'center', position: 'absolute'}} theme={theme} toggleTheme={toggleTheme} />
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
 				 onClick={routeChangeCreate} type="submit" value="Create"/>
-				<input className={`${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
 				 onClick={routeChangeHome} type="submit" value="Home"/>
 				</div>
 				<h1 className='f1'>RoboFriends</h1>
@@ -219,9 +219,9 @@ else if (state === 'loaded') {
 			<div className='tc'>	
 				<div style={{display: 'flex', flexDirection: 'row'}}>
 				<Toggle className=' tc' style={{alignSelf: 'center', position: 'absolute'}} theme={theme} toggleTheme={toggleTheme} />
-				<input className={` ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', textAlign: 'center' , cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute', cursor: 'pointer', top: '1rem', borderRadius: '12px'}} 
 				 onClick={routeChangeCreate} type="submit" value="Create"/>
-				<input className={` ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute',textAlign: 'center',left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px', background: `${colorVar}`}} 
+				<input className={` grow ${colorVar} ${colorVarText}`} style={{display: 'inline-block', textAlign: 'center', width: '7rem', alignSelf: 'flex-start', position: 'absolute',left: '7rem', cursor: 'pointer', top: '1rem', borderRadius: '12px'}} 
 				 onClick={routeChangeHome} type="submit" value="Home"/>
 				</div>
 				<h1 className='f1'>RoboFriends</h1>
--- src/CreateCard.js
+++ src/CreateCard.js
@@ -15,17 +15,17 @@ const CreateCard = ({generateCard, props, name, website, email, setName, setEmai
         <form className='flex flex-column items-center pa4' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <label classname= 'tc'> 
                 Name:</label >
-            <input className={`pa4-m grow outline tc ${colorVar}`} style={{width: '300'}}type="text" placeholder='New name here' onChange={e => setName(e.target.value)}/> 
+            <input className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}}type="text" placeholder='New name here' onChange={e => setName(e.target.value)}/> 
             
             <label classname= ''>
                 Email:</label>
-            <input className={`pa4-m grow outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New email here' onChange={e => setEmail(e.target.value)}/> 
+            <input className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New email here' onChange={e => setEmail(e.target.value)}/> 
             
             <label classname= ''>
                 Website:</label>
-            <input className={`pa4-m grow outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New website here'onChange={e => setWebsite(e.target.value)}/> 
+            <input className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New website here'onChange={e => setWebsite(e.target.value)}/> 
             
-            <button className={`b ph2 pv2 input-reset ba b--black grow pointer f6 dib ${colorVar}`} style={{marginTop: '1.5rem'}} onClick={(e) => submitValue(e) }>Submit</button>
+            <button className={`b ph2 pv2 input-reset ba b--white grow pointer f6 dib ${colorVar}`} style={{marginTop: '1.5rem'}} onClick={(e) => submitValue(e) }>Submit</button>
         </form>
 	</div>
     )
