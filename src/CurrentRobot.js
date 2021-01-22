785bdfb0b3fa8bf0748debbfab411832f39fa172
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-22 19:41:12 +0100

    1

--- src/CurrentRobot.js
+++ src/CurrentRobot.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Card from './Card';
 
-const CurrentRobot = (robots, newName) => {
+const CurrentRobot = ({robots, newName}) => {
     const [currentName, setCurrentName] = React.useState()
-    console.log(robots.newName);
+    console.log(robots.newName, 'here');
     React.useEffect(() => {
         //grabbing robot data
         const index = robots.length - 1
         const robotData = robots[index]
         if (robotData != null){
-            setCurrentName(<CurrentRobot className='f5'>Robot {robotData.newName} has been created!</CurrentRobot>)
+            setCurrentName(robotData.newName)
         }}, [robots])
     return (
         <div>
-            <f1>{currentName}</f1>
+            <h1 className='f5'>Robot {currentName} has been created!</h1>
              
               
         </div>
