90064b2a52b32fe3a1530c7b3fc95761727efd30
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-25 12:41:32 +0100

    1

--- src/CreateCard.js
+++ src/CreateCard.js
@@ -7,79 +7,123 @@ const CreateCard = ({setLastCard, newCard, props, name, website, email, setName,
     
     const submitValue = (e) => {
         e.preventDefault()
-        const isValid = validate();
-        if (isValid === true)
+        const isValidName = validateName();
+        const isValidEmail = validateEmail();
+        const isValidWebsite = validateWebsite();
+        if (isValidName === true && isValidEmail === true && isValidWebsite === true)
         {
             //let id2 = id1 + 1;
-        // setId1(id1 +1);
-        id1 = Math.floor(Math.random() * (1000 - 0 + 1));
-
-        const currentNewCard = {id: id1, newName: name, newEmail: email, newWebsite: website}
-        setNewCard(currentNewCard)
-        setRobots([...robots, currentNewCard]);
-        console.log(robots);
-        // setLastCard(currentNewCard);
-        setEmailError("");
-        setNameError("");
-        setWebsiteError("");
+            // setId1(id1 +1);
+            id1 = Math.floor(Math.random() * (1000 - 0 + 1));
+            const currentNewCard = {id: id1, newName: name, newEmail: email, newWebsite: website}
+            setNewCard(currentNewCard)
+            setRobots([...robots, currentNewCard]);
+            console.log(robots);
+            setEmailError("");
+            setNameError("");
+            setWebsiteError("");
+            setEmail("");
+            setName("");
+            setWebsite("");
         }
-        
-    };
-    function validate (nameError, emailError, websiteError) {
-        // if (!name.charAt(0) !== name.charAt(0).toUpperCase){
-        //     setNameError('First latter of the name must be uppercase');
-        //     return false;
-        // }
+        if (isValidName === true){
+            setNameError("");
+        }
+        if (isValidEmail === true){
+            setEmailError("");
+        }
+        if (isValidWebsite === true){
+            setWebsiteError("");
+        }
 
-        // if (nameError) {
-        //     //setEmailError(true);
-        //     return false;
-        // }
+    };
+    function validateName (nameError) {
+        if (name === '') {
+            setNameError('Field can not be empty');
+            return false;
+        }
         if (name.includes(' ')){
             setNameError('Robots can only have one name');
             return false;
         }
 
         if (nameError) {
-            //setEmailError(true);
+            
+            return false;
+        }
+        
+        return true;
+    }    
+    function validateEmail (emailError) {
+        if (email === '') {
+            setEmailError('Field can not be empty');
+            return false;
+        }
+        if (email.includes(' ')){
+            setEmailError('Invalid email');
+            return false;
+        } 
+ 
+        if (!email.includes('@')){
+            setEmailError('Invalid email');
             return false;
         }
 
-        if (!email.includes('@')){
-            setEmailError('invalid email');
+        if (!email.includes('.')){
+            setEmailError('Invalid email');
             return false;
         }
 
         if (emailError) {
-            //setEmailError(true);
             return false;
         }
+
+        return true;
+    }
+    function validateWebsite (websiteError) {
+        if (website === '') {
+            setWebsiteError('Field can not be empty');
+            return false;
+        }
+        if (website.includes(' ')){
+            setWebsiteError('Invalid website');
+            return false;
+        }
+        if (!website.includes('.')){
+            setWebsiteError('Invalid website');
+            return false;
+        }
+
+        if (websiteError) {
+            return false;
+        }
+
         return true;
     };
     return(
 	<div >
         <form className='flex flex-column items-center pa4' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <label className= 'tc'> 
+            <label className= 'tc' style={{width: '300', marginTop: '0.5rem'}}> 
                 Name:</label >
-            <input id='inputName' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}}type="text" placeholder='New name here' onChange={e => setName(e.target.value)}/>        
+            <input id='inputName' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} value={name} type="text" placeholder='New name here' onChange={e => setName(e.target.value)}/>        
             {
-                nameError ? (<div style={{font: "8", color: "red"}}>
+                nameError ? (<div style={{fontSize: "6", color: "red"}}>
                     {nameError}
                 </div>): null
             }
-            <label className= ''>
+            <label className= '' style={{width: '300', marginTop: '0.5rem'}}>
                 Email:</label>
-            <input id='inputEmail' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New email here' onChange={e => setEmail(e.target.value)}/>     
+            <input id='inputEmail' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} value={email} type="text" placeholder='New email here' onChange={e => setEmail(e.target.value)}/>     
             {
-                emailError ? (<div style={{font: "8", color: "red"}}>
+                emailError ? (<div style={{fontSize: "6", color: "red"}}>
                     {emailError}
                 </div>): null
             }
-            <label className= ''>
+            <label className= '' style={{width: '300', marginTop: '0.5rem'}}>
                 Website:</label>
-            <input id='inputWebsite' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} type="text" placeholder='New website here'onChange={e => setWebsite(e.target.value)}/> 
+            <input id='inputWebsite' className={`pa4-m grow b--white outline tc ${colorVar}`} style={{width: '300'}} value={website} type="text" placeholder='New website here'onChange={e => setWebsite(e.target.value)}/> 
             {
-                websiteError ? (<div style={{font: "8", color: "red"}}>
+                websiteError ? (<div style={{fontSize: "6", color: "red"}}>
                     {websiteError}
                 </div>): null
             }
