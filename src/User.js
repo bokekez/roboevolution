9a1e69e4ba908119b3c1dbb9e8995009873b3ed1
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-27 23:48:08 +0100

    1

--- src/User.js
+++ src/User.js
@@ -9,9 +9,10 @@ const User = ({setLogedUser, logedUser, user, setLogedin, logedin}) => {
     }
     if (user.username !== "" && logedin === true){
     return (
-        <div className='tc' style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-            <h3 className='f7' >{user.username}</h3>
-            <input className='dim  hover-white ' style={{outline: 'none', height: '0.8rem', width: '3rem', cursor: 'pointer', borderColor: 'transparent', fontSize: '0.5rem'}} 
+        <div className='tc' style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+            {/* <h3 className='f5' style={{width: '3rem'}} >{user.username}</h3> */}
+            <h3 className='f5 hover-white' style={{marginBottom: '0rem', marginRight: '0.7rem'}} >{user.username}</h3>
+            <input className='dim bg-transparent hover-white ' style={{marginRight: '0.3rem', display: 'inline-block', marginBottom: '0px', outline: 'none', marginLeft: '0rem', padding: '0', height: '0.8rem', width: '3rem', cursor: 'pointer', borderColor: 'transparent', fontSize: '0.5rem'}} 
 			onClick={logout} text="Log out" type="submit" value="Log out"/>
         </div>
         )
